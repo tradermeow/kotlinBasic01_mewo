4bee59a459b71e02c38870f9d2064940b0636d1a
Cat <jsl12395@naver.com>
2023-09-11 14:41:34 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/kotlinbasic01_mewo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.kotlinbasic01_mewo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
